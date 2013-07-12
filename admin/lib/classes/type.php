fdd93dd6ae4b3f10d21302b28792da6efc5a23df
Alexander Schlegel <sysix@sysix-coding.de>
2013-07-12 14:51:06 +0200

    type class. Typo Fix

--- admin/lib/classes/type.php
+++ admin/lib/classes/type.php
@@ -40,46 +40,46 @@ class type {
 					
 	}
 	
-	static public function post($var, $typ, $default = null) {
+	static public function post($var, $type, $default = null) {
 		
-		return self::checkVar($_POST, $var, $typ, $default);
-		
-	}
-	
-	static public function files($var, $default = null) {
-		
-		return self::checkVar($_FILES, $var, $typ, $default);
+		return self::checkVar($_POST, $var, $type, $default);
 		
 	}
 	
-	static public function super($var, $typ, $default = null) {
-	
-		return self::get($var, $typ, self::post($var, $typ, $default));
+	static public function files($var, $type, $default = null) {
+		
+		return self::checkVar($_FILES, $var, $type, $default);
 		
 	}
 	
-	static public function cookie($var, $typ, $default = null) {
-		
-		return self::checkVar($_COOKIE, $var, $typ, $default);
+	static public function super($var, $type, $default = null) {
+	
+		return self::get($var, $type, self::post($var, $type, $default));
 		
 	}
 	
-	static public function server($var, $typ, $default = null) {
+	static public function cookie($var, $type, $default = null) {
 		
-		return self::checkVar($_SERVER, $var, $typ, $default);
-		
-	}
-	
-	static public function session($var, $typ, $default = null) {
-		
-		return self::checkVar($_SESSION, $var, $typ, $default);
+		return self::checkVar($_COOKIE, $var, $type, $default);
 		
 	}
 	
-	static private function checkVar($global, $var, $typ, $default = null) {
+	static public function server($var, $type, $default = null) {
+		
+		return self::checkVar($_SERVER, $var, $type, $default);
+		
+	}
+	
+	static public function session($var, $type, $default = null) {
+		
+		return self::checkVar($_SESSION, $var, $type, $default);
+		
+	}
+	
+	static private function checkVar($global, $var, $type, $default = null) {
 		
 		if(array_key_exists($var, $global)) {
-			return self::cast($global[$var], $typ);	
+			return self::cast($global[$var], $type);	
 		}
 		
 		return $default;
