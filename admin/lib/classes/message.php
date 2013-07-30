fbf366aeb93b7745d4cbb7691b5eba38bd01c021
callofsorrow <info@grenzless.de>
2013-07-30 15:06:23 +0200

    eingerückt

--- admin/lib/classes/message.php
+++ admin/lib/classes/message.php
@@ -1,63 +1,63 @@
 <?php
 
 class message {
-  
-	static public function warning($message, $close = false) {
-		
-		$text = '<div class="alert alert-warning fade in">';
-		
-		if($close == true) {
+
+    static public function warning($message, $close = false) {
+    
+        $text = '<div class="alert alert-warning fade in">';
+        
+        if($close == true) {
         	$text .= '<button type="button" class="close" data-dismiss="alert">'.utf8_decode('×').'</button>';
-		}
-		
+        }
+        
         $text .= $message;
         $text .= '</div>';
-		
-		return $text;
-	}
-	
-	static public function info($message, $close = false) {
-		
-		$text = '<div class="alert alert-info fade in">';
-		
-		if($close == true) {
+        
+        return $text;
+    }
+    
+    static public function info($message, $close = false) {
+    
+        $text = '<div class="alert alert-info fade in">';
+        
+        if($close == true) {
         	$text .= '<button type="button" class="close" data-dismiss="alert">'.utf8_decode('×').'</button>';
-		}
-		
+        }
+        
         $text .= $message;
         $text .= '</div>';
-		
-		return $text;
-	}
-	
-	static public function danger($message, $close = false) {
-		
-		$text = '<div class="alert alert-danger fade in">';
-		
-		if($close == true) {
+        
+        return $text;
+    }
+    
+    static public function danger($message, $close = false) {
+    
+        $text = '<div class="alert alert-danger fade in">';
+        
+        if($close == true) {
         	$text .= '<button type="button" class="close" data-dismiss="alert">'.utf8_decode('×').'</button>';
-		}
-		
+        }
+        
         $text .= $message;
         $text .= '</div>';
-		
-		return $text;
-	}
-	
-	static public function success($message, $close = false) {
-		
-		$text = '<div class="alert alert-success fade in">';
-		
-		if($close == true) {
+        
+        return $text;
+    }
+    
+    static public function success($message, $close = false) {
+    
+        $text = '<div class="alert alert-success fade in">';
+        
+        if($close == true) {
         	$text .= '<button type="button" class="close" data-dismiss="alert">'.utf8_decode('×').'</button>';
-		}
-		
+        }
+        
         $text .= $message;
         $text .= '</div>';
-		
-		return $text;
-	}
-	
+        
+        return $text;
+    }
+
 }
 
 ?>
