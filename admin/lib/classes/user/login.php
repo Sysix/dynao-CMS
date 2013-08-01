fd72da754a97129203c6e2a683ca87b4c4521c23
Alexander Schlegel <sysix@sysix-coding.de>
2013-08-01 15:00:40 +0200

    class userLogin - Abfragen hinzugefügt

--- admin/lib/classes/user/login.php
+++ admin/lib/classes/user/login.php
@@ -4,18 +4,20 @@ class userLogin {
 
 	protected $email;
 	protected $password;
+	protected $isLogin;
+	protected $userID;
 	
 	public function __construct() {
 	
-		if(isset($_GET["logout"])) {
+		if(!is_null(type::get('logout', 'string'))) {
 	
 			$this->logout();
 			   
-		} elseif(!empty($_SESSION["userID"]) && ($_SESSION["logged_in"] == 1)) {
+		} elseif($this->checkLogin()) {
 	
 			$this->loginSession();
 	
-		} elseif (isset($_GET["login"])) {
+		} elseif (!is_null(type::get('login', 'string'))) {
 	
 			$this->loginPost();
 	
@@ -26,20 +28,76 @@ class userLogin {
 	//wenn Session stimmt, status auf true
 	protected function loginSession() {
 	
-		$this->logged_in = true;
+		$this->isLogin = true;
 	
+	}
+	
+	// Überprüfen ob Session richtig gesetzt 
+	protected function checkLogin() {
+	
+		$session = type::session('login', 'string', false);
+		
+		if(!$session)
+			return false;
+		
+		// Session[0] = ID; session[1} PW in sha1	
+		$session = explode('||', $session);
+		
+		$sql = new sql();
+		$sql->result('SELECT id FROM user WHERE `id` = '.$session[0].' AND `password` = "'.self::hash($session[1]).'"');	
+			
+		if(!$sql->num()) {
+			
+			$this->logout();
+			return false;
+		
+		}
+		
+		return true;		
+		
 	}
 	
 	//Einloggen
 	protected function loginPost() {
-	
-		if (!empty($_POST["email"]) && !empty($_POST["password"])) {
 		
-			$sql = new sql();
-			$sql->result('SELECT * FROM user');
-			echo $sql->num();
+		$email = type::post('email', 'string');
+		$password = type::post('password', 'string');
 		
+		// Formular ganz abgesendet?
+		if(is_null($email) || is_null($password)) {
+			
+			echo message::info('Formular nicht vollständig gesendet!');
+			$this->logout();
+			return;
+			
 		}
+		
+		$sql = new sql();
+		$sql->query('SELECT password, id FROM user WHERE `email` = '.$email);
+		
+		// Username mit E-Mail vorhanden?
+		if(!$sql->num()) {
+		
+			echo message::danger('Kein Benutzer mit der E-Mail-Adresse "'.$email.'" gefunden');
+			$this->logout();
+			return;
+			
+		}
+				
+		$sql->result();
+		
+		// Password nicht gleich?
+		if(!self::checkPassword($password, $sql->get('password'))) {
+			
+			echo message::danger('Das angebene Passwort ist falsch');
+			$this->logout();
+			return;
+			
+		}
+		
+		$this->loginSession();
+		$this->userID = $sql->get('id');
+		$_SESSION['login'] = $sql->get('id').'||'.self::hash($password);
 	
 	}
 	
@@ -61,14 +119,14 @@ class userLogin {
 	public function logout() {   
 	
 		session_destroy();
-		$this->logged_in = false;
+		$this->isLogin = false;
 		
 	}
 	
 	//status wiedergeben
-	public function getStatus() {
+	public function isLogged() {
 		
-		return $this->logged_in;
+		return $this->isLogin;
 		
 	}
 
