6c6051d321a45404423fcc3311f9dedc8018e7b1
Alexander Schlegel <sysix@sysix-coding.de>
2013-08-12 13:26:45 +0200

    cache klasse in autoload für Verzeichnis

--- admin/lib/classes/autoload.php
+++ admin/lib/classes/autoload.php
@@ -4,7 +4,9 @@ class autoload {
 	
 	
 	static $classes = array();
+	static $dirs = array();
 	static $registered = false;
+	static $isNewCache = false;
 	
 	
 	static public function register() {
@@ -16,6 +18,11 @@ class autoload {
 		if(spl_autoload_register(array(__CLASS__, 'autoloader')) === false) {
 			//throw new Exception();
 		}
+		
+		self::loadCache();
+		
+		
+        register_shutdown_function(array(__CLASS__, 'saveCache'));
 		
 		self::$registered = true;
 		
@@ -34,24 +41,61 @@ class autoload {
 			return true;	
 		}
 		
+		
 		preg_match_all("/(?:^|[A-Z])[a-z]+/", $class, $treffer);
 		
 		$classPath = '';
+		$x = 0;
 		foreach($treffer[0] as $dir) {
-			$classPath .= '/'.strtolower($dir);
+			
+			if($x) {
+				$classPath .= DIRECTORY_SEPARATOR.strtolower($dir);
+			} else {
+				$classPath .= strtolower($dir);
+			}
+			
+			$x++;
 		}
 		
-		self::$classes[] = dir::classes($classPath.'.php');
-		
-		include_once(dir::classes($classPath.'.php'));
+		self::addClass($classPath.'.php');
 		
 		return class_exists($class);
 		
 	}
 	
-	static public function getClasses() {
+	static public function saveCache() {
 		
-		return self::$classes;	
+		if(self::$isNewCache) {
+			
+			$cacheFile = cache::getFileName(0, 'autoloadcache');
+			
+			cache::write(json_encode(array(self::$classes, self::$dirs)), $cacheFile);
+			self::$isNewCache = false;
+			
+		}
+		
+	}
+	
+	static protected function loadCache() {
+		
+		$cacheFile = cache::getFileName(0, 'autoloadcache');
+		
+		if(!cache::exist($cacheFile, 3600))
+			return;
+				
+		list(self::$classes, self::$dirs) = json_decode(cache::read($cacheFile), true);
+		
+		foreach(self::$classes as $class) {
+			include_once($class);
+		}
+	}
+	
+	// Hinzufügen von Klassen
+	static protected function addClass($path) {
+		
+		self::$classes[] = dir::classes($path);
+		
+		include_once(dir::classes($path));
 		
 	}
 	
@@ -61,6 +105,13 @@ class autoload {
 			//throw new Exception;	
 		}
 		
+		// Schon eingescannt
+		if(in_array($dir, self::$dirs)) {
+			return;	
+		}
+		
+		self::$dirs[] = $dir;
+		
 		$files = scandir(dir::classes($dir));
 		
 		foreach($files as $file) {			
@@ -68,8 +119,8 @@ class autoload {
 			if(in_array($file, array('.', '..')))
 				continue;
 				
-			self::$classes[] = dir::classes($dir.DIRECTORY_SEPARATOR.$file);
-			include_once(dir::classes($dir.DIRECTORY_SEPARATOR.$file));
+			self::addClass($dir.DIRECTORY_SEPARATOR.$file);
+			self::$isNewCache = true;
 			
 		}
 		
