ffc5dc3e5d440794cfc978bda9ca520dad1f72b8
Alexander Schlegel <sysix@sysix-coding.de>
2013-08-13 11:13:25 +0200

    structure add default page

--- admin/pages/structure.php
+++ admin/pages/structure.php
@@ -6,84 +6,118 @@ $id = type::super('id', 'int', 0);
 if($action == 'delete') {
 	
 		$sql = new sql();
-		$sql->setTable('news');
+		$sql->setTable('structure');
 		$sql->setWhere('id='.$id);
 		$sql->delete();
 		
-		$action = '';
+		echo message::success('Artikel erfolgreich gelöscht');
 		
 }
 
-if($action == 'add' || $action == 'edit') {
+if($action == 'online') {
 
-	$form = new form('news','id='.$id,'index.php');
+	$sql = new sql();
+	$sql->query('SELECT online FROM structure WHERE id='.$id)->result();
 	
-	$field = $form->addTextField('title', $form->get('title'));
-	$field->fieldName('News-Titel');
+	$online = ($sql->get('online')) ? 0 : 1;
 	
-	$field = $form->addTextareaField('text', $form->get('text'));
-	$field->fieldName('Infotext');
+	$sql->setTable('structure');
+	$sql->setWhere('id='.$id);
+	$sql->addPost('online', $online);
+	$sql->update();
 	
-	if($action == 'edit') {
-		$form->addHiddenField('id', $id);
+	echo message::success('Status erfoglreich geändert');
+	
+}
+
+if(in_array($action, array('save-add', 'save-edit'))) {
+	
+	$sql = new sql();
+	$sql->setTable('structure');
+	$sql->setWhere('id='.$id);
+	$sql->getPosts(array('name'=>'string','sort'=>'int'));
+	
+	if($action == 'save-edit') {
+		$sql->update();	
+	} else {
+		$sql->save();	
+	}
+}
+
+$table = new table();
+
+$table->addCollsLayout('*, 50, 250');
+	
+$table->addRow()
+->addCell('Artikel')
+->addCell('Pos')
+->addCell('Aktion');
+
+$table->addSection('tbody');
+
+$cacheFileName = cache::getFileName(1, 'structure');
+
+if(cache::exist($cacheFileName) && !in_array($action, array('edit', 'add'))) {
+	
+	echo cache::read($cacheFileName);
+	
+} else {
+	
+	if(in_array($action, array('edit', 'add'))) {
+		echo '<form method="post" action="index.php">';
+		echo '<input type="hidden" name="action" value="save-'.$action.'">';
+		echo '<input type="hidden" name="page" value="structure">';
 	}
 	
-	if($form->isSubmit()) {
+	if($action == 'add') {
+	
+		$table->addRow()
+		->addCell('<input type="text" name="name" value="" />')
+		->addCell('<input type="text" name="sort" value="" />')
+		->addCell('<button type="submit">Artikel speichern</button>');	
 		
-		$form->addPost('date', time());
+	}
 
-	}	
-	
-	echo $form->show();
-	
-}
-	
-if($action == '') {
 
-	$table = new table();
-	
-	$table->addRow()
-	->addCell('Titel')
-	->addCell('Datum')
-	->addCell('Aktion');
-	
-	$table->addSection('tbody');
-	
-	$cacheFileName = cache::getFileName(1, 'news');
-	
-	if(cache::exist($cacheFileName)) {
+	$table->setSql('SELECT * FROM structure ORDER BY sort ASC');
+	while($table->isNext()) {
 		
-		echo cache::read($cacheFileName);
-		
-	} else {
-	
-	
-		$table->setSql('SELECT * FROM news ORDER BY date DESC');
-		while($table->isNext()) {
-			
-			$id = $table->get('id');
-			
-			$edit = '<a href="'.url::backend('structure', array('action'=>'edit', 'id'=>$id)).'" class="btn btn-small  btn-default">'.lang::get('edit').'</a>';
-			$delete = '<a href="'.url::backend('structure', array('action'=>'delete', 'id'=>$id)).'" class="btn btn-small btn-danger">'.lang::get('delete').'</a>';
+		if($action == 'edit') {
 			
 			$table->addRow()
-			->addCell($table->get('title'))
-			->addCell(date('d.m.Y', $table->get('date')))	
-			->addCell($edit.$delete);
+			->addCell('<input type="text" name="name" value="'.$table->get('name').'" />')
+			->addCell('<input type="text" name="sort" value="'.$table->get('sort').'" />')
+			->addCell('<input type="hidden" name="id" value="'.$table->get('id').'" /><button type="submit">Artikel speichern</button>');
 			
-			$table->next();	
+		} else {
+			
+			$edit = '<a href="'.url::backend('structure', array('action'=>'edit', 'id'=>$table->get('id'))).'" class="btn btn-small  btn-default">'.lang::get('edit').'</a>';
+			$delete = '<a href="'.url::backend('structure', array('action'=>'delete', 'id'=>$table->get('id'))).'" class="btn btn-small btn-danger">'.lang::get('delete').'</a>';
+			
+			$online = ($table->get('online')) ? 'online' : 'offline';
+			$online = '<a href="'.url::backend('structure', array('action'=>'online', 'id'=>$table->get('id'))).'" class="btn btn-small structure-'.$online.'">'.$online.'</a>';
+		
+			$table->addRow()
+			->addCell($table->get('name'))
+			->addCell($table->get('sort'))	
+			->addCell($edit.' '.$delete.' '.$online);
+			
 		}
 		
-		extension::get('structure_table', $table);
-		
-		$show = $table->show();
-		
-		cache::write($show, $cacheFileName);
-		
-		echo $show;
-	
+		$table->next();	
 	}
 	
+	$show = $table->show();
+	
+	cache::write($show, $cacheFileName);
+	
+	echo $show;
+	
+	if(in_array($action, array('edit', 'add'))) {
+		echo '</form>';
+	}
+
 }
+
 
 ?>
