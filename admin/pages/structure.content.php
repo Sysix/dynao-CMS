d812911c620266ef9823d46c1d1a04726a4678c0
Alexander Schlegel <sysix@sysix-coding.de>
2013-08-14 10:19:08 +0200

    Struktur Content Style (HTML)

--- admin/pages/structure.content.php
+++ admin/pages/structure.content.php
@@ -1,3 +1,45 @@
 <?php
-
+layout::addJsCode(' $(function() {
+$( "#sortable" ).sortable();
+$( "#sortable" ).disableSelection();
+});');
 ?>
+<ul id="sortable">
+
+	<li>
+		<div class="panel">
+		  <div class="panel-heading">
+			<h3 class="panel-title pull-left">Modul #4</h3>
+			<div class="pull-right btn-group">
+				<a class="btn btn-small structure-online">ON</a>
+				<a class="btn btn-default btn-small icon-cog"></a>
+				<a class="btn btn-default btn-small icon-edit-sign"></a>
+				<a class="btn btn-danger btn-small icon-trash"></a>
+			</div>
+			<div class="clearfix"></div>
+		  </div>
+		  <div class="panel-body">
+			Content
+		  </div>
+		</div>
+	</li>
+	
+	<li>
+		<div class="panel">
+		  <div class="panel-heading">
+			<h3 class="panel-title pull-left">Modul #3</h3>
+			<div class="pull-right btn-group">
+				<a class="btn btn-small structure-online">ON</a>
+				<a class="btn btn-default btn-small icon-cog"></a>
+				<a class="btn btn-default  btn-small icon-edit-sign"></a>
+				<a class="btn btn-danger btn-small icon-trash"></a>
+			</div>
+			<div class="clearfix"></div>
+		  </div>
+		  <div class="panel-body">
+			Content
+		  </div>
+		</div>
+	</li>
+	
+</ul>
