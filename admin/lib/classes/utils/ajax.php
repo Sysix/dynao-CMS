59b4a308e34505c39b1ccd4fa00a8009b2104d42
Alexander Schlegel <sysix@sysix-coding.de>
2013-08-15 22:46:02 +0200

    add ajax class + module sortierung

---
+++ admin/lib/classes/utils/ajax.php
@@ -0,0 +1,37 @@
+<?php
+
+class ajax {
+	
+	static $return = array();
+	
+	public static function is() {
+	
+		return type::server('HTTP_X_REQUESTED_WITH', 'string', '') == 'XMLHttpRequest';
+		
+	}
+	
+	public static function addReturn($text) {
+		
+		self::$return[] = $text;
+		
+	}
+	
+	public static function getReturn() {
+	
+		return implode('<br />', self::$return);
+		
+	}
+	
+	public static function convertPOST() {
+		
+		if(ajax::is()) {
+			
+			parse_str($_POST['ajaxGet'], $post);
+			$_POST = array_merge($_POST, $post);
+			
+		}
+		
+	}
+}
+
+?>
