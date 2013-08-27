50e37841411d0b315dea24cb6bf24c8704c04652
Alexander Schlegel <sysix@sysix-coding.de>
2013-08-27 20:41:43 +0200

    page addon - online/active Buttons

--- admin/pages/addons.php
+++ admin/pages/addons.php
@@ -2,7 +2,49 @@
 $action = type::super('action', 'string');
 $addon = type::super('addon', 'string');
 
-backend::addSubnavi('Übersicht',	url::backend('addons'), 				'archive');
+backend::addSubnavi('Übersicht',	url::backend('addons'), 	'archive');
+
+if($action == 'online') {
+
+	$addonClass= new addon($addon);	
+	$online = ($addonClass->isOnline()) ? 0 : 1;
+	
+	$sql = new sql();
+	$sql->setTable('addons');
+	$sql->setWhere('`name` = "'.$addon.'"');
+	$sql->addPost('online', $online);
+	
+	if(!$online)
+		$sql->addPost('active', 0);
+	
+	$sql->update();
+	
+	echo message::success('Addon erfolgreich gespeichert');
+	
+}
+
+if($action == 'active') {
+	
+	$addonClass = new addon($addon);	
+	$active = ($addonClass->isActive()) ? 0 : 1;
+	
+	if(!$addonClass->isOnline()) {
+		
+		echo message::danger('Bitte installieren Sie das Addon '.$addon.' zuerst');
+			
+	} else {
+	
+		$sql = new sql();
+		$sql->setTable('addons');
+		$sql->setWhere('`name` = "'.$addon.'"');
+		$sql->addPost('active', $active);
+		$sql->update();
+		
+		echo message::success('Addon erfolgreich gespeichert');
+		
+	}
+	
+}
 
 $table = new table();
 
@@ -24,15 +66,27 @@ foreach($addons as $dir) {
 	
 	$curAddon = new addon($dir);
 	
-	$online = $curAddon->isOnline() ? 'installiert' : 'nicht installiert';
-	$active = $curAddon->isActive() ? 'aktiviert' : 'nicht aktiviert';
+	$online_url = url::backend('addons', array('addon'=>$dir, 'action'=>'online'));
+	
+	if($curAddon->isOnline()) {
+		$online = '<a href="'.$online_url.'" class="btn btn-sm structure-online">installiert</a>';
+	} else {
+		$online = '<a href="'.$online_url.'" class="btn btn-sm structure-offline">nicht installiert</a>';
+	}
+	$active_url = url::backend('addons', array('addon'=>$dir, 'action'=>'active'));
+	
+	if($curAddon->isActive()) {
+		$active = '<a href="'.$active_url.'" class="btn btn-sm structure-online">aktiviert</a>';
+	} else {
+		$active = '<a href="'.$active_url.'" class="btn btn-sm structure-offline">nicht aktiviert</a>';
+	}
 	
 	$table->addRow()
 	->addCell('<a href="" class="icon-question"></a>')
 	->addCell($curAddon->get('name').' <small>'.$curAddon->get('version').'</small>')
 	->addCell($online)
 	->addCell($active)
-	->addCell('Entfernen');
+	->addCell('<a href="#" class="btn btn-sm btn-danger">Entfernen</a>');
 		
 }
 
