6019f4afa25a2929b458c7d091478a586f0b881b
Alexander Schlegel <sysix@sysix-coding.de>
2013-09-30 11:49:22 +0200

    Addon Mediamanager

---
+++ admin/addons/mediamanager/config.php
@@ -0,0 +1,17 @@
+<?php
+
+if(!class_exists('media')) {
+	require(dir::addon('mediamanager', 'lib/classes/media.php'));
+}
+
+if(!dyn::has('extensions')) {
+	
+	$imageExtensions = array("gif", "jpeg", "jpg", "png", "bmp");
+	$videoExtensions = array("3gp", "avi", "flv", "m4v", "mov", "mp4", "mpg", "wmv", "mkv", "mpeg");
+	$audioExtensions = array("mp3", "wma", "m4a");
+	
+	dyn::add('extensions', array($imageExtensions, $videoExtensions, $audioExtensions), true);
+	
+}
+
+?>
