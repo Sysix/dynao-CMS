feabd5be3172fe3f6f3bea300463b03a2b9639e8
Alexander Schlegel <sysix@sysix-coding.de>
2013-10-08 16:26:19 +0200

    mediamanager add subpages

--- admin/addons/mediamanager/page/media.php
+++ admin/addons/mediamanager/page/media.php
@@ -1,82 +1,22 @@
 <?php
 
+backend::addSubnavi('Medien verwalten',				url::backend('media', array('subpage'=>'files')));
+backend::addSubnavi('Medienkategorien verwalten',	url::backend('media', array('subpage'=>'category')));
+backend::addSubnavi('Dateien Synchronisieren',		url::backend('media', array('subpage'=>'sync')));
+
 $action = type::super('action', 'string', '');
 $id = type::super('id', 'int', 0);
 
-if($action == 'add' || $action == 'edit') {
-	
-	$form = new form('media', 'id='.$id, 'index.php');
-	$form->addFormAttribute('enctype', 'multipart/form-data');
-	
-	$field = $form->addTextField('title', $form->get('title'));
-	$field->fieldName('Titel');
-	
-	$field = $form->addTextareaField('description', $form->get('description'));
-	$field->fieldName('Beschreibung');
-	
-	$field = $form->addRawField('<input type="file" name="file" />');
-	$field->fieldName('Datei auswählen');
-	
-	if($action == 'edit') {
-		$form->addHiddenField('id', $id);
-	}
-	
-	if($form->isSubmit()) {
-		
-		$file = type::files('file', '');
-		if(is_uploaded_file($file['tmp_name'])) {
-			
-			$fileName = mediaUtils::fixFileName($file['name']);
-			
-			if(move_uploaded_file($file['tmp_name'], dir::media($fileName))) {
-				
-				$form->addPost('filename', $fileName);
-				$form->addPost('size', filesize(dir::media($fileName)));
-				
-			} else {
-				
-				$form->setSave(false);
-				echo message::warning($file['name'].' konnte nicht gespeichert werden.');
-				
-			}
-			
-		}
-		
-		
-	}
-	
-	echo $form->show();	
-	
-}
+$allowPages = array('files', 'category', 'sync');
+$allowPages = extension::get('page_subpages', $allowPages);
 
-if($action == '') {
+if(in_array($subpage, $allowPages)) {
 	
-	$table = new table();
-	$table->setSql('SELECT id FROM '.sql::table('media'));
-	$table->addRow()
-	->addCell()
-	->addCell('Titel')
-	->addCell('Endung')
-	->addCell('Aktion');
-	$table->addSection('tbody');
-	while($table->isNext()) {
-			
-		$media = new media($table->get('id'));
-		
-		$edit = '<a href="'.url::backend('media', array('action'=>'edit', 'id'=>$table->get('id'))).'" class="btn btn-sm  btn-default">'.lang::get('edit').'</a>';
-		$delete = '<a href="'.url::backend('media', array('action'=>'delete', 'id'=>$table->get('id'))).'" class="btn btn-sm btn-danger">'.lang::get('delete').'</a>';
-			
-		$table->addRow()
-		->addCell('<img src="'.$media->getPath().'" style="max-width:50px; max-height:50px" />')
-		->addCell($media->get('title'))
-		->addCell($media->getExtension())
-		->addCell('<span class="btn-group">'.$edit.$delete.'</span>');
-		
-		$table->next();	
-		
-	}
+	include_once(dir::addon('mediamanager', 'page/media.'.$subpage.'.php'));
 	
-	echo $table->show();
+} else {
+	
+	include_once(dir::addon('mediamanager', 'page/media.'.$allowPages[0].'.php'));
 	
 }
 
