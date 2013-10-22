b61d948d6ca55d8bd6a231bf163d72a55c98cdac
Alexander Schlegel <sysix@sysix-coding.de>
2013-10-22 12:48:59 +0200

    meta articels bugfix + sort

--- admin/addons/metainfos/page/meta.articles.php
+++ admin/addons/metainfos/page/meta.articles.php
@@ -2,9 +2,25 @@
 
 $formTypes = array('text', 'textarea', 'select', 'radio', 'checkbox', 'DYN_LINK', 'DYN_MEDIA', 'DYN_LINK_LIST', 'DYN_MEDIA_LIST');
 
+if(ajax::is()) {
+	
+	$sort = type::post('array', 'array');
+	
+	$sql = new sql();
+	$sql->setTable('metainfos');
+	foreach($sort as $s=>$id) {
+		$sql->setWhere('id='.$id);
+		$sql->addPost('sort', $s+1);
+		$sql->update();	
+	}
+	
+	ajax::addReturn(message::success('Sortierung erfolgreich übernommen', true));
+	
+}
+
 if($action == 'add' || $action == 'edit') {
 	
-	$form = new form('metainfo', 'id='.$id, 'index.php');
+	$form = new form('metainfos', 'id='.$id, 'index.php');
 	
 	$field = $form->addTextField('label', $form->get('label'));
 	$field->fieldName('Beschreibung');
@@ -12,7 +28,7 @@ if($action == 'add' || $action == 'edit') {
 	$field = $form->addTextField('name', $form->get('name'));
 	$field->fieldName('Name');
 	
-	$field = $form->addSelectField('type', $form->get('type'));
+	$field = $form->addSelectField('formtype', $form->get('formtype'));
 	$field->fieldName('Feldtyp');
 	foreach($formTypes as $type) {
 		$field->add($type, $type);	
@@ -20,14 +36,14 @@ if($action == 'add' || $action == 'edit') {
 	
 	$field = $form->addTextField('default', $form->get('default'));
 	$field->fieldName('Standardwert');
-	$field->addSuffix('<small>Bei Mehrauswahl mit einen <b>|</b> trennen</small>');
+	$field->setSuffix('<small>Bei Mehrauswahl mit einen <b>|</b> trennen</small>');
 	
 	$field = $form->addTextareaField('params', $form->get('params'));
 	$field->fieldName('Parameter');
 	
 	$field = $form->addTextareaField('attributes', $form->get('attributes'));
 	$field->fieldName('HTML-Attribute');
-	$field->addSuffix('<small>Beispiel:<br /> style=color:red multiple=multiple class=my_css_class</small>');
+	$field->setSuffix('<small>Beispiel:<br /> style=color:red multiple=multiple class=my_css_class</small>');
 	
 	$form->addHiddenField('type', 'articles');
 	
@@ -37,17 +53,16 @@ if($action == 'add' || $action == 'edit') {
 	
 	echo $form->show();	
 	
-	
 }
 
 if($action == '') {
 	
 	echo '<a href="'.url::backend('meta', array('subpage'=>'articles', 'action'=>'add')).'" class="btn btn-sm btn-primary pull-right">'.lang::get('add').'</a>';
 	
-	$table = new table();
+	$table = new table(array('class'=>array('js-sort')));
 	$table->setSql('SELECT * FROM '.sql::table('metainfos').' WHERE `type` = "articles"');
 	
-	$table->addRow()->addCell('Name')->addCell('Aktion');
+	$table->addRow()->addCell()->addCell('Name')->addCell('Aktion');
 	
 	$table->addSection('tbody');
 	while($table->isNext()) {
@@ -55,7 +70,8 @@ if($action == '') {
 		$edit = '<a href="'.url::backend('meta', array('subpage'=>'articles', 'action'=>'edit', 'id'=>$table->get('id'))).'" class="btn btn-sm  btn-default">'.lang::get('edit').'</a>';
 		$delete = '<a href="'.url::backend('meta', array('subpage'=>'articles', 'action'=>'delete', 'id'=>$table->get('id'))).'" class="btn btn-sm btn-danger">'.lang::get('delete').'</a>';
 		
-		$table->addRow()
+		$table->addRow(array('data-id'=>$table->get('id')))
+		->addCell('<i class="icon-sort"></i>')
 		->addCell($table->get('name'))
 		->addCell('<span class="btn-group">'.$edit.$delete.'</span>');
 		
