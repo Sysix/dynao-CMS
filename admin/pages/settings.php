cd0eb78e86169e7578ae7fe6f2e9d873083734d1
Alexander Schlegel <sysix@sysix-coding.de>
2013-11-07 10:43:13 +0100

    add save in page settings

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -1,18 +1,57 @@
 <?php
 	backend::addSubnavi('General',	url::backend(''),	'eye');
-	backend::addSubnavi('Media',	url::backend(''),	'picture-o');
-	backend::addSubnavi('SEO',	url::backend(''),	'group');
 ?>
 
 <div class="row">
 	
-    <div class="col-lg-5">
+    <div class="col-lg-8">
         <div class="panel panel-default">
             <div class="panel-heading">
             	<h3 class="panel-title">General</h3>
             </div>
             <div class="panel-body">
-            	...
+            	<?php
+				
+				$form = new form('user', 'id=1', 'index.php');
+				$form->setSave(false);
+				$form->delButton('save');
+				
+				if($form->isSubmit()) {
+					
+					$url = 'http://'.str_replace('http://', '', $form->get('url'));
+					$endSlash = substr($url, -1, 1);
+					
+					if($endSlash != '/') {
+						$url .= '/';	
+					}
+					
+					dyn::add('hp_name', $form->get('name'), true);
+					dyn::add('hp_url', $url, true);
+					dyn::add('lang', $form->get('lang'), true);
+					dyn::save();
+					
+				}
+				
+				$field = $form->addTextField('name', dyn::get('hp_name'));
+				$field->fieldName('Name der Website');
+				
+				$field = $form->addTextField('url', dyn::get('hp_url'));
+				$field->fieldName('URL der Website');
+				
+				$field = $form->addSelectField('lang', dyn::get('lang'));
+				$field->fieldName('Backend Sprache');
+							
+				$handle = opendir(dir::backend('lib'.DIRECTORY_SEPARATOR.'lang'.DIRECTORY_SEPARATOR));
+				while($file = readdir($handle)) {
+						
+						if(in_array($file, array('.', '..')))
+							continue;
+						
+						$field->add($file, $file);
+				}
+				
+				echo $form->show();
+				?>
             </div>
         </div>
     </div>
@@ -22,30 +61,34 @@
             <div class="panel-heading">
             	<h3 class="panel-title">System</h3>
             </div>
-            <table class="table table-striped table-hover">
-            	<thead>
-                	<th>Type</th>
-                    <th>Value</th>
-                </thead>
-                <tbody>
-                	<tr>
-                    	<td>Language frontend</td>
-                        <td>de_de</td>
-                    </tr>
-                	<tr>
-                    	<td>Cache lifetime</td>
-                        <td>60</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    
-    <div class="col-lg-3">
-        <div class="panel panel-default">
-            <div class="panel-body">
-            	...
-            </div>
+			<?php
+			
+			$sql = new sql();
+			$sql->query('SELECT VERSION()')->result();
+			
+			$table = new table(array('class'=> array('table', 'table-spriped', 'table-hover')));
+			$table->addRow()
+			->addCell('Type')
+			->addCell('Value');
+			
+			$table->addSection('tbody');
+			
+			$table->addRow()
+			->addCell('Dynao Version')
+			->addCell(dyn::get('version'));
+			
+			$table->addRow()
+			->addCell('PHP Version')
+			->addCell(phpversion());
+			
+			$table->addRow()
+			->addCell('MYSQL Version')
+			->addCell($sql->get('VERSION()'));
+			
+			
+			echo $table->show();
+			
+			?>
         </div>
     </div>
     
