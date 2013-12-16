de03de8348dfe1b50d55c99f483dfa0a5774ef1b
callofsorrow <info@grenzless.de>
2013-12-16 13:18:06 +0100

    install weiter, fehler bei form

--- install/pages/overview.php
+++ install/pages/overview.php
@@ -2,6 +2,18 @@
 
     <div class="col-lg-8">
     
+    	<?php
+			
+			foreach(['json', 'session', 'curl', 'mysqli', 'pcre'] as $extension) {
+
+            	if(!extension_loaded($extension)) {
+            		echo message::danger(sprintf(lang::get('php_ext'), $extension));
+					$error = true;
+            	}
+                    
+            }
+		?>
+    
         <div class="panel panel-default">
         
                 <div class="panel-heading">
@@ -12,12 +24,13 @@
                     <?php
 						
 						$form = form_install::factory('', '', 'index.php');
+						$form->setSave(false);
 						
 						$field = $form->addTextField('name', $form->get('name'));
-						$field->fieldName('Seitenname');
+						$field->fieldName(lang::get('settings_name_of_site'));
 						
 						$field = $form->addTextField('url', $form->get('url'));
-						$field->fieldName('URL der Seite');
+						$field->fieldName(lang::get('settings_url_of_site'));
 						
 						$field = $form->addSelectField('lang', dyn::get('lang'));
 						$field->fieldName(lang::get('settings_backend_lang'));
@@ -29,6 +42,17 @@
 									continue;
 								
 								$field->add($file, $file);
+						}
+						
+						if($form->isSubmit()) {
+							if($error)
+								echo message::danger('error');
+							else {
+								dyn::add('hp_name', $form->get('hp_name'), true);
+								dyn::add('hp_url', $url, true);
+								dyn::add('lang', $form->get('lang'), true);
+								header('Location: index.php?page=database');
+							}
 						}
 						
 						echo $form->show();
@@ -46,68 +70,77 @@
         <div class="panel panel-default">
         
                 <div class="panel-heading">
-                    <h3 class="panel-title"><?php echo lang::get('general'); ?></h3>
+                    <h3 class="panel-title"><?php echo lang::get('settings'); ?></h3>
                 </div>
-                <div class="panel-body">
                 
-                	<?php
-					
-					if (version_compare(phpversion(), '5.4', '<')) {
-						echo 'PHP Version 5.4 wird mindestens benötigt<br />';						
-					} else {
-						echo 'PHP Version - OK<br />';	
-					}
-					
-					echo '<br />';
-					
-					$writeable = [
-						dir::cache(),
-						dir::cache('page'.DIRECTORY_SEPARATOR),
-						dir::backend('addons'.DIRECTORY_SEPARATOR),
-						dir::backend('lib'.DIRECTORY_SEPARATOR.'config.json')
-					];
-					
-					function stripPath($file) {
-						
-						return str_replace(dir::base(), '', $file);
-							
-					}
-					
-					foreach($writeable as $file) {
-						
-						if(is_file($file)) {
-							
-							if(is_writeable($file)) {
-								echo 'Datei '.stripPath($file).' - OK<br />';
-							} else {
-								echo 'Datei '.stripPath($file).' - Fehler<br />';
-							}
-							
-							
-						} elseif(is_dir($file)) {
-							
-							if(is_writeable($file)) {
-								echo 'Ordner '.stripPath($file).' - OK<br />';
-							} else {
-								echo 'Ordner '.stripPath($file).' - Fehler<br />';
-							}
-							
-						}
-						
-					}
-					
-					echo '<br />';
-					
-					foreach(['json', 'session', 'curl', 'mysqli', 'pcre'] as $extension) {
-						
-						if(!extension_loaded($extension)) {
-							echo 'PHP Extension "'.$extension.'" konnte nicht geladen werden<br />';	
-						}
-						
-					}
-					?>
+				<?php
                 
-                </div>
+                $table = table::factory();
+                
+                $table->addRow()
+                ->addCell(lang::get('type'))
+                ->addCell(lang::get('status'));
+                
+				$table->addSection('tbody');
+				
+                if (version_compare(phpversion(), '5.4', '<')) {		
+                    $table->addRow()
+                    ->addCell(lang::get('php_version'))
+                    ->addCell('<span class="label label-danger">'.lang::get('php_version_54').'</span>');				
+                } else {
+                    $table->addRow()
+                    ->addCell(lang::get('php_version'))
+                    ->addCell('<span class="label label-success">'.lang::get('ok').'</span>');	
+                }
+                
+                $writeable = [
+                    dir::cache(),
+                    dir::cache('page'.DIRECTORY_SEPARATOR),
+                    dir::backend('addons'.DIRECTORY_SEPARATOR),
+                    dir::backend('lib'.DIRECTORY_SEPARATOR.'config.json')
+                ];
+                
+                function stripPath($file) {
+                    
+                    return str_replace(dir::base(), '', $file);
+                        
+                }
+                
+                foreach($writeable as $file) {
+                    
+                    if(is_file($file)) {
+                        
+                        if(is_writeable($file)) {
+							$table->addRow()
+                    		->addCell(stripPath($file))
+                    		->addCell('<span class="label label-success">'.lang::get('ok').'</span>');
+                        } else {
+							$table->addRow()
+                    		->addCell(stripPath($file))
+                    		->addCell('<span class="label label-danger">'.lang::get('chmod_755').'</span>');	
+							$error = true;
+                        }
+                        
+                        
+                    } elseif(is_dir($file)) {
+                        
+                        if(is_writeable($file)) {
+							$table->addRow()
+                    		->addCell(stripPath($file))
+                    		->addCell('<span class="label label-success">'.lang::get('ok').'</span>');
+                        } else {
+							$table->addRow()
+                    		->addCell(stripPath($file))
+                    		->addCell('<span class="label label-danger">'.lang::get('chmod_755').'</span>');
+							$error = true;
+                        }
+                        
+                    }
+                    
+                }
+                
+                echo $table->show();
+                ?>
         
         </div>
     
